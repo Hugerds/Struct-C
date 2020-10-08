1429bc9e742836e086a61048980d3b2dd7e76e64
Hugo Esteves <hugo.esteves@unesp.br>
2020-10-08 14:34:57 -0300

    Ex2

--- Struct_ex2.c
+++ Struct_ex2.c
@@ -31,11 +31,34 @@ void negar(a, b) {
 	}
 }
 
+
+void contSomar(int mmc, int a, int b, int c, int d, int num1, int num2) {
+	int primeiro, segundo;
+	primeiro = (mmc/b)*a;
+	segundo = (mmc/d)*c;
+	printf ("A soma entre %d/%d+%d/%d e igual a: %d/%d\n\n", a, b, c, d, primeiro+segundo, mmc);
+}
+
+void mmc(int num1, int num2, int a, int c) {
+    int resto, b, d, mmc, aux, i;
+    b = num1;
+    d = num2;
+    for (i = 2; i <= num2; i++) {
+        aux = num1 * i;
+        if ((aux % num2) == 0) {
+            mmc = aux;
+            i = num2 + 1;
+        }
+    }
+    contSomar(mmc, a, b, c, d, num1, num2);
+}
+
 void somar(a, b) {
 	printf ("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
 	int c, d;
 	printf ("Digite a outra fracao que deseja somar por %d/%d: ", a, b);
 	scanf("%d %d", &c, &d);
+	mmc(b, d, a, c);
 	
 }
 
